0fb93ccbcccbb74cc2bc28b2fd2fd785c8549393
Gestalt <gestalt.the.protogen@gmail.com>
2025-09-06 12:28:01 -0600

    removed old bones to prevent server errors

---
+++ kemono/resources/assets/kemono/shapes/entity/kemono/main/remove-bad-bones.py
@@ -0,0 +1,37 @@
+﻿import json
+
+bad_bones = [
+    "b_TorsoUpper",
+    "b_TorsoLower",
+    "b_ArmUpperL",
+    "b_ArmUpperR",
+    "b_ArmLowerL",
+    "b_ArmLowerR",
+    "b_ItemAnchor",
+    "b_ItemAncorL"
+]
+
+
+# Load JSON files
+with open('kemono0.json', 'r') as f1:
+    data = json.load(f1)
+
+# Process each animation in file1
+for anim in data["animations"]:
+    code = anim["code"]
+
+    for keyframe in anim["keyframes"]:
+        elements = keyframe.get("elements", {})
+        newElems = {}
+
+        for key, value in elements.items():
+            if key not in bad_bones:
+                newElems[key] = value
+
+        # Assign updated elements back
+        keyframe["elements"] = newElems
+
+# Write the merged result
+with open('cleaned-kemono0.json', 'w') as fout:
+    json.dump(data, fout, indent=2)
+    
